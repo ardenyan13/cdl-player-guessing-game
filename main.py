9755270bbb786d23d4e574ead9fa8e770f94f0bb
ardenyan13 <148033490+ardenyan13@users.noreply.github.com>
2024-09-30 18:44:04 -0700

    add guess player endpoint

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+players = ["Shotzzy", "Pred", "Kenny", "Dashy"]
 
 @app.get("/start")
 def start_game():
     return {"message": "Call of Duty League Player Guessing Game"}
+
+@app.get("/guess/{player_name}")
+def guess_player(player_name: str):
+    if player_name in players:
+        return {"message": f"Correct {player_name} is the player"}
+    
+    raise HTTPException(status_code=404)
